c88ec42872a49a273fd1c4ad9517f52831520db7
Lenin Antonio Ormaza Alvarez <lenin.antonio.ormaza12@hotmail.com>
2020-12-13 00:55:05 -0500

    Usando un archivo Data de Open Weather Map en ligar de la api City-Ceo-Location

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-const axios = require('axios');
-
 const argv = require('./config/yargs').argv;
-let apiKey = '29e392191139af6aefee6809f24f79e9';
 
-const instance = axios.create({
-    baseURL: `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${argv.direccion}`
-});
+const { getLugarLatLng } = require('./lugar/lugar');
 
-instance.get()
-    .then(result => console.log(result.data))
-    .catch(error => console.log(`Error!!! ${error}`));
+getLugarLatLng(argv.direccion)
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
---
+++ lugar/lugar.js
@@ -0,0 +1,17 @@
+const getLugarLatLng = (direccion) => {
+    return new Promise((resolve, reject) => {
+        let listCity = require('../data/citylist.json');
+        if (!listCity || listCity.lenth === 0) {
+            reject("No se pudieron obtener los datos")
+        } else {
+            let city = listCity.filter(city => city.name.includes(direccion));
+            if (!city) {
+                reject(`La ciudad ${direccion} no fue encontrada`);
+            } else {
+                resolve(city);
+            }
+        }
+    })
+};
+
+module.exports = { getLugarLatLng };
